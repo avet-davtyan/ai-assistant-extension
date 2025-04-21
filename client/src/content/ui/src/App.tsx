9fbea3f248a09029b18e0dc9fae2ca2be224b83d
Avet Davtyan <32107783+avet-davtyan@users.noreply.github.com>
2025-04-21 22:13:52 +0400

    chore(element-info-collection-handler): transfer info collection to mousemove listener (#15)

--- client/src/content/ui/src/App.tsx
+++ client/src/content/ui/src/App.tsx
@@ -1,26 +1,16 @@
+import PromptBox from "./components/prompt-box/PromptBox";
 import { usePromptBoxStore } from "./store/prompt-box-store";
-import { Button } from "antd";
 
 const App = () => {
 
   const {
-    left,
-    top,
     visible,
   } = usePromptBoxStore();
 
   return (
-    <div
-    id="annoying-div"
-    >
-      <Button style={{
-        visibility: visible ? "visible" : "hidden",
-        position: "fixed",
-        left: left,
-        top: top,
-        zIndex: 1000
-      }}>Annoying Button</Button>
-    </div>
+    <>
+    {visible && <PromptBox />}
+    </>
   );
 };
 
---
+++ client/src/content/ui/src/components/prompt-box/PromptBox.tsx
@@ -0,0 +1,37 @@
+import { Button, Flex, Input } from "antd";
+import { usePromptBoxStore } from "../../store/prompt-box-store";
+
+const PromptBox = () => {
+
+  const {
+    left,
+    top,
+    visible,
+    hide,
+  } = usePromptBoxStore();
+
+  const handleClosePromptBox = () => {
+    hide();
+  }
+  
+  return (
+    <div style={
+      {
+        position: "fixed",
+        left: left,
+        top: top,
+        zIndex: 1000
+      }
+    }>
+      <Flex vertical style={{
+        gap: "10px",
+        padding: "10px",
+      }}>
+        <Button type="primary" onClick={handleClosePromptBox}>Close Prompt Box</Button>
+        <Input.TextArea />
+      </Flex>
+    </div>
+  )
+}
+
+export default PromptBox;
